65d71ab2be2ad548f700d91fc2e50ae302214ed0
Jennifer <jmfcodes@riseup.net>
2017-08-14 21:46:01 -0700

    added separate wordslist.txt

--- wordgame.py
+++ wordgame.py
@@ -1,31 +1,16 @@
-"""a hangman-style game; future updates will allow choosing how long the word should be!"""
+"""a hangman-style game; future updates will allow choosing how long the word should be!
+
+wordfile.txt source:
+http://www.ef.com/english-resources/english-vocabulary/top-3000-words/
+"""
 
 import random
 
-def pick_word(difficulty):         # picks from a list of words
-    # would like to update to pull from a txt file
-    e = ['mouse', 'house', 'ducks', 'coffee', 'beans', 'glasses', 'flower', 'tape', 'doorway', 'window', 'cars', 'faint', 'sandbox', 'peanut', 'parsley', 'boxes', 'darling', 'blackness', 'barking', 'fallen', 'farce', 'whisker']
-    m = ['outlast', 'witch', 'nerves', 'faint', 'sandy', 'vines', 'batteries', 'razors', 'vegan', 'chorus',
-    'parsed', 'turtle', 'giraffe', 'minced', 'sewing', 'visible', 'double', 'trouble', 'tasty', 'grease', 'soiled', 'strength', 'rolling', 'jumping', 'jiggle', 'rugged', 'frisky', 'fauna']
-    h = ['intensified', 'rigging', 'swept', 'jackboot', 'copyright', 'alliterating', 'slashed', 'pittance', 'electricity', 'beater', 'chalkboard', 'woodsy', 'outlaw', 'sluggish', 'arbitrate', 'devoured', 'dismissive', 'epiphany', 'illegible', 'oscillate', 'evangelize', 'veganism', 'annihilate', 'ataxia', 'collegiate', 'paleolithic', 'glucose', 'cautioned', 'supplier', 'impounded', 'primitive', 'immobility']
-
-    if difficulty == 'e':
-        answer = random.choice(e).upper()  # need to specify short length
-    elif difficulty == 'm':
-        answer = random.choice(m).upper()  # need to specify medium length
-    elif difficulty == 'h':
-        answer = random.choice(h).upper()  # need to specify long length
-    else:
-        print 'Invalid choice! Why don\'t you play the easy level?'
-        answer = random.choice(e).upper()  # need to specify short length
-    return answer
-
-    #word_list = open("wordlist.txt","w+")
-    #word = random.choice(word_list).upper()
-    #word_list.close()
-    #return random.choice(words).upper()
-    #return random.choice(word_list).upper()
-    #need to choose based on len(word)
+def pick_word():         # picks from a list of words
+    
+    wordfile = [line.strip() for line in open('wordslist.txt')]
+    return random.choice(wordfile).upper()
+    
 
 def check(answer,guesses,guess):
     status = ''
@@ -46,14 +31,14 @@ def check(answer,guesses,guess):
     return status
 
 def main():
-    difficulty = raw_input('Welcome to non-violent Hangman! Choose (e)asy, (m)edium, or (h)ard:  ')
-    answer = pick_word(difficulty)
+    print('\nWelcome to non-violent Hangman! Let\'s begin!')
+    answer = pick_word()
     choices = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     guesses = []
     guessed = False
     print 'This word contains {} letters.'.format(len(answer))
     while not guessed:
-        text = '\nEnter 1 letter or {}-letter word.\nYour choices are: {}:  '.format(len(answer), choices)
+        text = 'Enter 1 letter or {}-letter word.\nYour choices are: {}:  '.format(len(answer), choices)
         guess = raw_input(text)
         guess = guess.upper()
         choices = choices.replace(guess, '-')
